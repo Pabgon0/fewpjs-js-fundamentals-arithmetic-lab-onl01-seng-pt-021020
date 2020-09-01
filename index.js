0f268c681fd2c84a80ef00789a5c032b032e0bb9
Pablo Gonzalez <pabgon0@gmail.com>
2020-09-01 22:14:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const num1 = 2,
       num2 = 31,
-      multiply = num1 * num2,
-      random = Math.floor(Math.random() * 10) + 1,
+      multiply = num1 * num2
+const random = Math.floor(Math.random() * 10) + 1
       
