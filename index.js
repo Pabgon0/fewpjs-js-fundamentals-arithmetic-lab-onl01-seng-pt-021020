c0f591864c32ff0d585596f7f18c2c6e22937e73
Pablo Gonzalez <pabgon0@gmail.com>
2020-09-01 22:16:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const num1 = 2,
       num2 = 31,
       multiply = num1 * num2
 const random = Math.floor(Math.random() * 10) + 1
-const mod = 16 % 4
+const mod = 24 % 6
 const max = Math.max(2, 5, 11, 20)
